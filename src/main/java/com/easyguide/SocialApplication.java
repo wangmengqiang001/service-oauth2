6c5fdc5c6c22714d492e875de834707f4af133db
wangmengqiang <wangmengqiang001@126.com>
2018-06-11 06:31:59 +0000

    organize imports

--- src/main/java/com/easyguide/SocialApplication.java
+++ src/main/java/com/easyguide/SocialApplication.java
@@ -1,20 +1,40 @@
 package com.easyguide;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.web.bind.annotation.RestController;
-import java.security.Principal;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
 @SpringBootApplication
 @EnableOAuth2Sso
 @RestController
 public class SocialApplication extends WebSecurityConfigurerAdapter{
 
+  @Autowired
+  OAuth2ClientContext oauth2ClientContext;
+  
    @RequestMapping("/user")
   public Principal user(Principal principal) {
     return principal;
